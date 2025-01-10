6b7e40a7fd1dc8e2ab88442fe532f739b0f871f9
Abhishek Savant <abhisheksavant0@gmail.com>
2025-01-10 11:28:16 +0530

    Updated search component with new style

--- src/Components/search-box/search.jsx
+++ src/Components/search-box/search.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './search.css';
 
 export const Search = ({placeholder, handleChange}) => (
+<header className = "search-header">
 <input
           className='search'
           type="search"
           placeholder={placeholder}
           onChange={handleChange}
 />
+<div className="close">
+<span className='front'></span>
+<span className='back'></span>
+</div>
+</header>
 )
